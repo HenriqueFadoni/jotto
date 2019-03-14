c2830ef0e00c6fbc50e8e59fdea0a4a65a2e4ff7
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-14 12:40:14 -0300

    Creating integration test for Actions and Reducers

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCounter } from '../../helpers/index';
+
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
     GUESS_WORD: 'GUESS_WORD'
@@ -11,6 +13,19 @@ export const actionTypes = {
 
 export const guessWord = guessedWord => {
     return function(dispatch, getState) {
-        
+        const secretWord = getState().secretWord;
+        const letterMatchCount = getLetterMatchCounter(guessedWord, secretWord);
+
+        dispatch({
+            type: actionTypes.GUESS_WORD,
+            payload: {
+                guessedWord,
+                letterMatchCount
+            }
+        });
+
+        if(guessedWord === secretWord) {
+            dispatch({ type: actionTypes.CORRECT_GUESS });
+        }
     };
 };
