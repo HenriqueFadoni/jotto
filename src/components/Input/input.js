9cd5d51a2f028edf95c632d38645f70c5fd5e6c4
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-16 16:58:53 -0300

    Demo of Completed App

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -15,6 +15,8 @@ export class Input extends Component {
         if (guessedWord && guessedWord.length > 0) {
             this.props.guessWord(guessedWord);
         }
+
+        this.inputBox.current.value = '';
     }
 
     render() {
