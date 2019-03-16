7ad1e46dc9da96a4ab5baad10e29fb623dff3d4a
Henrique Fadoni <henriquefadonii@gmail.com>
2019-03-16 16:54:55 -0300

    Create tests and functionality for guessWord argument from input Box

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -4,27 +4,36 @@ import { connect } from 'react-redux';
 import { guessWord } from '../../store/actions/index';
 
 export class Input extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.inputBox = React.createRef();
     }
+
+    submitGuessedWord = event => {
+        event.preventDefault();
+        const guessedWord = this.inputBox.current.value;
+        if (guessedWord && guessedWord.length > 0) {
+            this.props.guessWord(guessedWord);
+        }
+    }
+
     render() {
         const contents = this.props.success
             ? null
             : (
                 <form className="form-inline">
-                    <input 
+                    <input
                         data-test="input-box"
                         ref={this.inputBox}
                         className="mb-2 mx-sm-3"
                         id="word-guess"
                         type="text"
-                        placeholder="enter guess"/>
+                        placeholder="enter guess" />
                     <button
                         data-test="submit-button"
                         type="submit"
                         className="btn btn-primary mb-2"
-                        onClick={() => this.props.guessWord('train')}
+                        onClick={this.submitGuessedWord}
                         type="submit">
                         Submit
                     </button>
